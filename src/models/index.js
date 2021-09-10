58066ca906768383ac3faa99602ed79348f8fd38
Igor Tkachenko <stiles.2126@gmail.com>
2021-09-10 20:40:13 +1000

    added associations for models

--- src/models/index.js
+++ src/models/index.js
@@ -4,6 +4,12 @@ const LessonStudents = require('./lessonStudents.model');
 const Students = require('./students.model');
 const Teachers = require('./teachers.model');
 
+Lessons.belongsToMany(Students, { through: LessonStudents, foreignKey: 'lesson_id' });
+Students.belongsToMany(Lessons, { through: LessonStudents, foreignKey: 'student_id' });
+
+Lessons.belongsToMany(Teachers, { through: LessonTeachers, foreignKey: 'lesson_id' });
+Teachers.belongsToMany(Lessons, { through: LessonTeachers, foreignKey: 'teacher_id' });
+
 module.exports = {
     Lessons,
     LessonTeachers,

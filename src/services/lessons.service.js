426af152fbb61b25a038f6ab48028857edcf5b00
Igor Tkachenko <stiles.2126@gmail.com>
2021-09-12 22:39:13 +1000

    added lessons service 'createLessons' and added validation

--- src/services/lessons.service.js
+++ src/services/lessons.service.js
@@ -1,14 +1,13 @@
 /* eslint-disable no-nested-ternary */
 const { Op, Sequelize } = require('sequelize');
+const sequelize = require('../db');
 const models = require('../models');
-
-function validateParams(params) {
-    if (params.date) { return true; }
-    return true;
-}
+const validator = require('./validators/lessons.validator');
+const ApiError = require('../utils/apiError');
 
 module.exports.getLessonsByParams = async (params) => {
-    if (validateParams(params)) {
+    const validParams = validator.getParams(params);
+    if (validParams.validated) {
         const result = models.Lessons.findAll({
             attributes: {
                 include: [[Sequelize.literal(`(SELECT COUNT(*)::INTEGER FROM lesson_students AS lesson_students  WHERE 
@@ -72,5 +71,94 @@ module.exports.getLessonsByParams = async (params) => {
         });
         return result;
     }
-    return false;
+    throw new ApiError('Validation Error', 400, validParams.error);
+};
+
+module.exports.createLessons = async (body) => {
+    const validParams = validator.postBody(body);
+    if (validParams.validated) {
+        let lessonsIds = [];
+        let lessonsCount = 300;
+        const lessonsArray = [];
+        const teachersIds = [];
+
+        // поиск существующих учителей
+        try {
+            await models.Teachers.findAll({
+                attributes: ['id'],
+                where: { id: { [Op.in]: body.teachersIds.map((x) => parseInt(x, 10)) } },
+            }).then((ids) => {
+                teachersIds.push(...ids.map((id) => id.dataValues.id));
+            });
+            if (!teachersIds.length) throw Error();
+        } catch (err) {
+            throw new ApiError('Validation Error', 400, 'These \'teachersIds\' Not Found');
+        }
+
+        // расчет количества записей по дате
+        const currentDate = new Date(body.firstDate);
+        const maxDate = new Date(currentDate);
+        maxDate.setFullYear(currentDate.getFullYear() + 1);
+
+        if (body.lessonsCount) {
+            if (body.lessonsCount < lessonsCount) {
+                lessonsCount = body.lessonsCount;
+            }
+        }
+
+        if (body.lastDate) {
+            const lastDate = new Date(body.lastDate);
+            if (lastDate < maxDate) {
+                maxDate.setDate(lastDate);
+            }
+        }
+
+        // подготовка данных 'lessons'
+        for (let i = 0; i < lessonsCount;) {
+            if (body.days.includes(currentDate.getDay())) {
+                lessonsArray.push({
+                    date: currentDate.toISOString().split('T')[0],
+                    title: body.title,
+                    status: 0,
+                });
+                i++;
+            }
+            currentDate.setDate(currentDate.getDate() + 1);
+            if (currentDate > maxDate) break;
+        }
+
+        // транзакция на сохранение уроков и учителей
+        const t = await sequelize.transaction();
+        try {
+            await models.Lessons.bulkCreate(
+                lessonsArray,
+                {
+                    transaction: t,
+                    returning: ['id'],
+                },
+            ).then((lessons) => {
+                lessonsIds = lessons.map((lesson) => lesson.dataValues.id);
+            });
+
+            // подготовка данных 'lesson_teachers'
+            const lessonTeachersArray = [];
+            lessonsIds.map((lessonId) => teachersIds.map((tId) => lessonTeachersArray.push({
+                teacher_id: tId,
+                lesson_id: lessonId,
+            })));
+
+            await models.LessonTeachers.bulkCreate(
+                lessonTeachersArray,
+                { transaction: t },
+            );
+
+            await t.commit();
+        } catch (err) {
+            await t.rollback();
+            throw new Error(err);
+        }
+
+        return lessonsIds;
+    }
+    throw new ApiError('Validation Error', 400, validParams.error);
 };

406f0d3666a8ead344d1135b126414239ac49245
Igor Tkachenko <stiles.2126@gmail.com>
2021-09-12 22:37:27 +1000

    added tests for POST '/api/lessons'

--- test/lessonsRequests.test.js
+++ test/lessonsRequests.test.js
@@ -111,9 +111,69 @@ describe('GET /api/', () => {
 });
 
 describe('POST /api/lessons', () => {
-    it('expect status 400 (with incorrect body)', async () => {
+    it('expect status 200 and numbers array by lessonsCount', async () => {
         const res = await request(app)
-            .post('/api/lessons');
+            .post('/api/lessons').send({
+                teachersIds: [1, 2, 3],
+                title: 'Test title',
+                days: [0, 1, 2],
+                firstDate: '2019-01-01',
+                lessonsCount: 4,
+            });
+        expect(res.status).toEqual(200);
+        expect(res.body).toEqual(expect.arrayContaining([
+            expect.any(Number),
+        ]));
+    });
+
+    it('expect status 200 and numbers array by lastDate', async () => {
+        const res = await request(app)
+            .post('/api/lessons').send({
+                teachersIds: [1, 2, 3],
+                title: 'Test title',
+                days: [0, 1, 2],
+                firstDate: '2019-01-01',
+                lastDate: '2023-01-01',
+            });
+        expect(res.status).toEqual(200);
+        expect(res.body).toEqual(expect.arrayContaining([
+            expect.any(Number),
+        ]));
+    });
+
+    it('expect status 400 (with incorrect teachersIds)', async () => {
+        const res = await request(app)
+            .post('/api/lessons').send({
+                teachersIds: [-9, 0],
+                title: 'Test title',
+                days: [1, 2],
+                firstDate: '2019-01-01',
+                lessonsCount: 2,
+            });
+        expect(res.status).toEqual(400);
+    });
+
+    it('expect status 400 (with incorrect days)', async () => {
+        const res = await request(app)
+            .post('/api/lessons').send({
+                teachersIds: [1, 2],
+                title: 'Test title',
+                days: [5, 7, 8],
+                firstDate: '2019-01-01',
+                lessonsCount: 2,
+            });
+        expect(res.status).toEqual(400);
+    });
+
+    it('expect status 400 (with incorrect firstDate)', async () => {
+        const res = await request(app)
+            .post('/api/lessons').send({
+                teachersIds: [1, 2],
+                title: 'Test title',
+                days: [1, 2],
+                firstDate: 'right now',
+                lessonsCount: 2,
+            });
         expect(res.status).toEqual(400);
     });
 });
